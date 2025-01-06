863a2780fb98942609ff3242d30fcfe30a5af3e9
Tom Zurales <tomzurales@gmail.com>
2025-01-06 11:53:16 -0800

    Updated EuRoC_to_SDF to use new sensor format

--- modules/util/EuRoC_to_SDF/src/EuRoC_to_SDF.cc
+++ modules/util/EuRoC_to_SDF/src/EuRoC_to_SDF.cc
@@ -7,62 +7,64 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#include "sdf/sdf.h"
+#include "sdf/generator.h"
 #include "sdf/plugins/camera_pinhole_radtan.h"
 #include "sdf/plugins/imu_6dof_allan.h"
-#include "sdf/plugins/position_leica.h"
 #include "sdf/transform.h"
 
-void processBody(std::filesystem::path bodyPath, std::shared_ptr<SDF::SDF> sdf)
+void processBody(std::filesystem::path bodyPath, std::shared_ptr<SDF::Generator> generator)
 {
 }
 
-void processCamera(std::filesystem::path cameraPath, YAML::Node cameraConfig, std::shared_ptr<SDF::SDF> sdf)
+void processCamera(std::filesystem::path cameraPath, YAML::Node cameraConfig, std::shared_ptr<SDF::Generator> generator)
 {
   std::shared_ptr<SDF::sensors::CameraPinholeRadTan> camera(new SDF::sensors::CameraPinholeRadTan());
-  camera->name = cameraPath.filename().string();
-  camera->transform = SDF::Transform(cameraConfig["T_BS"]["data"].as<std::vector<float>>());
+  auto props = camera->getProps();
+  props->name = cameraPath.filename().string();
+  // props->transform = SDF::Transform(cameraConfig["T_BS"]["data"].as<std::vector<float>>());
 
-  camera->notes = cameraConfig["comment"].as<std::string>();
-  camera->rate = cameraConfig["rate_hz"].as<float>();
-  camera->width = cameraConfig["resolution"][0].as<uint32_t>();
-  camera->height = cameraConfig["resolution"][1].as<uint32_t>();
+  props->notes = cameraConfig["comment"].as<std::string>();
+  props->rate = cameraConfig["rate_hz"].as<float>();
+  props->width = cameraConfig["resolution"][0].as<uint32_t>();
+  props->height = cameraConfig["resolution"][1].as<uint32_t>();
   std::vector<float> intrinsics = cameraConfig["intrinsics"].as<std::vector<float>>();
-  camera->fx = intrinsics[0];
-  camera->fy = intrinsics[1];
-  camera->cx = intrinsics[2];
-  camera->cy = intrinsics[3];
+  props->fx = intrinsics[0];
+  props->fy = intrinsics[1];
+  props->cx = intrinsics[2];
+  props->cy = intrinsics[3];
   std::vector<float> distortion = cameraConfig["distortion_coefficients"].as<std::vector<float>>();
-  camera->k1 = distortion[0];
-  camera->k2 = distortion[1];
-  camera->p1 = distortion[2];
-  camera->p2 = distortion[3];
+  props->k1 = distortion[0];
+  props->k2 = distortion[1];
+  props->p1 = distortion[2];
+  props->p2 = distortion[3];
 
-  camera->lazyLoad = true;
-
+  // props->lazyLoad = true;
   for (std::filesystem::path imagePath : std::filesystem::directory_iterator(cameraPath / "data"))
   {
-    std::shared_ptr<SDF::sensors::CameraPinholeRadTan::Data> cameraData = std::make_shared<SDF::sensors::CameraPinholeRadTan::Data>(std::stoull(imagePath.stem()), imagePath);
-    camera->data.push_back(cameraData);
+    std::shared_ptr<SDF::sensors::CameraPinholeRadTanData> cameraData = std::make_shared<SDF::sensors::CameraPinholeRadTanData>();
+    cameraData->timestamp = std::stoull(imagePath.filename().stem().string());
+    cameraData->image = cv::imread(imagePath.string());
+    camera->addData(cameraData);
   }
 
-  sdf->addSensor(camera);
+  generator->addSensor(camera);
 }
 
-void processIMU(std::filesystem::path imuPath, YAML::Node imuConfig, std::shared_ptr<SDF::SDF> sdf)
+void processIMU(std::filesystem::path imuPath, YAML::Node imuConfig, std::shared_ptr<SDF::Generator> generator)
 {
   std::shared_ptr<SDF::sensors::Imu6dofAllan> imu(new SDF::sensors::Imu6dofAllan());
-  imu->name = imuPath.filename().string();
-  imu->transform = SDF::Transform(imuConfig["T_BS"]["data"].as<std::vector<float>>());
+  auto props = imu->getProps();
+  props->name = imuPath.filename().string();
+  // props->transform = SDF::Transform(imuConfig["T_BS"]["data"].as<std::vector<float>>());
 
-  imu->notes = imuConfig["comment"].as<std::string>();
-  imu->rate = imuConfig["rate_hz"].as<float>();
-  imu->gyroscope_noise_density = imuConfig["gyroscope_noise_density"].as<float>();
-  imu->gyroscope_random_walk = imuConfig["gyroscope_random_walk"].as<float>();
-  imu->accelerometer_noise_density = imuConfig["accelerometer_noise_density"].as<float>();
-  imu->accelerometer_random_walk = imuConfig["accelerometer_random_walk"].as<float>();
+  props->notes = imuConfig["comment"].as<std::string>();
+  props->rate = imuConfig["rate_hz"].as<float>();
+  props->gyroscope_noise_density = imuConfig["gyroscope_noise_density"].as<float>();
+  props->gyroscope_random_walk = imuConfig["gyroscope_random_walk"].as<float>();
+  props->accelerometer_noise_density = imuConfig["accelerometer_noise_density"].as<float>();
+  props->accelerometer_random_walk = imuConfig["accelerometer_random_walk"].as<float>();
 
-  imu->lazyLoad = false;
+  // props->lazyLoad = false;
 
   std::ifstream imuDataFile(imuPath / "data.csv");
   if (!imuDataFile.is_open())
@@ -78,7 +80,7 @@ void processIMU(std::filesystem::path imuPath, YAML::Node imuConfig, std::shared
     {
       continue;
     }
-    std::shared_ptr<SDF::sensors::Imu6dofAllan::Data> imuData = std::make_shared<SDF::sensors::Imu6dofAllan::Data>();
+    std::shared_ptr<SDF::sensors::Imu6dofAllanData> imuData = std::make_shared<SDF::sensors::Imu6dofAllanData>();
     std::istringstream ss(line);
     std::string token;
     std::getline(ss, token, ',');
@@ -95,75 +97,75 @@ void processIMU(std::filesystem::path imuPath, YAML::Node imuConfig, std::shared
     imuData->wy = std::stof(token);
     std::getline(ss, token, ',');
     imuData->wz = std::stof(token);
-    imu->data.push_back(imuData);
+    imu->addData(imuData);
   }
 
-  sdf->addSensor(imu);
+  generator->addSensor(imu);
 }
 
-void processLeica(std::filesystem::path leicaPath, YAML::Node leicaConfig, std::shared_ptr<SDF::SDF> sdf)
+void processLeica(std::filesystem::path leicaPath, YAML::Node leicaConfig, std::shared_ptr<SDF::Generator> generator)
 {
-  std::shared_ptr<SDF::sensors::PositionLeica> leica(new SDF::sensors::PositionLeica());
-  leica->name = leicaPath.filename().string();
-  leica->transform = SDF::Transform(leicaConfig["T_BS"]["data"].as<std::vector<float>>());
-  leica->notes = leicaConfig["comment"].as<std::string>();
+  // std::shared_ptr<SDF::sensors::PositionLeica> leica(new SDF::sensors::PositionLeica());
+  // leica->name = leicaPath.filename().string();
+  // leica->transform = SDF::Transform(leicaConfig["T_BS"]["data"].as<std::vector<float>>());
+  // leica->notes = leicaConfig["comment"].as<std::string>();
 
-  leica->lazyLoad = false;
+  // leica->lazyLoad = false;
 
-  std::ifstream leicaDataFile(leicaPath / "data.csv");
-  if (!leicaDataFile.is_open())
-  {
-    std::cerr << "Could not open data file for Leica: " << leicaPath.string() << std::endl;
-    return;
-  }
+  // std::ifstream leicaDataFile(leicaPath / "data.csv");
+  // if (!leicaDataFile.is_open())
+  // {
+  //   std::cerr << "Could not open data file for Leica: " << leicaPath.string() << std::endl;
+  //   return;
+  // }
 
-  // TODO: Write an actual CSV parser
-  for (std::string line; std::getline(leicaDataFile, line);)
-  {
-    if (line.empty() || line[0] == '#')
-    {
-      continue;
-    }
-    std::shared_ptr<SDF::sensors::PositionLeica::Data> leicaData = std::make_shared<SDF::sensors::PositionLeica::Data>();
-    std::istringstream ss(line);
-    std::string token;
-    std::getline(ss, token, ',');
-    leicaData->timestamp = std::stoull(token);
-    std::getline(ss, token, ',');
-    leicaData->pos_x = std::stof(token);
-    std::getline(ss, token, ',');
-    leicaData->pos_y = std::stof(token);
-    std::getline(ss, token, ',');
-    leicaData->pos_z = std::stof(token);
-    leica->data.push_back(leicaData);
-  }
+  // // TODO: Write an actual CSV parser
+  // for (std::string line; std::getline(leicaDataFile, line);)
+  // {
+  //   if (line.empty() || line[0] == '#')
+  //   {
+  //     continue;
+  //   }
+  //   std::shared_ptr<SDF::sensors::PositionLeica::Data> leicaData = std::make_shared<SDF::sensors::PositionLeica::Data>();
+  //   std::istringstream ss(line);
+  //   std::string token;
+  //   std::getline(ss, token, ',');
+  //   leicaData->timestamp = std::stoull(token);
+  //   std::getline(ss, token, ',');
+  //   leicaData->pos_x = std::stof(token);
+  //   std::getline(ss, token, ',');
+  //   leicaData->pos_y = std::stof(token);
+  //   std::getline(ss, token, ',');
+  //   leicaData->pos_z = std::stof(token);
+  //   leica->data.push_back(leicaData);
+  // }
 
-  sdf->addSensor(leica);
+  // generator->addSensor(leica);
 }
 
-void processGroundTruth(std::filesystem::path groundTruthPath, YAML::Node groundTruthConfig, std::shared_ptr<SDF::SDF> sdf)
+void processGroundTruth(std::filesystem::path groundTruthPath, YAML::Node groundTruthConfig, std::shared_ptr<SDF::Generator> generator)
 {
 }
 
-void processSensor(std::filesystem::path sensorPath, std::shared_ptr<SDF::SDF> sdf)
+void processSensor(std::filesystem::path sensorPath, std::shared_ptr<SDF::Generator> generator)
 {
   YAML::Node sensorConfig = YAML::LoadFile(sensorPath / "sensor.yaml");
   std::string sensor_type = sensorConfig["sensor_type"].as<std::string>();
   if (sensor_type == "camera")
   {
-    processCamera(sensorPath, sensorConfig, sdf);
+    processCamera(sensorPath, sensorConfig, generator);
   }
   else if (sensor_type == "imu")
   {
-    processIMU(sensorPath, sensorConfig, sdf);
+    processIMU(sensorPath, sensorConfig, generator);
   }
   else if (sensor_type == "visual-inertial")
   {
-    processGroundTruth(sensorPath, sensorConfig, sdf);
+    processGroundTruth(sensorPath, sensorConfig, generator);
   }
   else if (sensor_type == "position")
   {
-    processLeica(sensorPath, sensorConfig, sdf);
+    processLeica(sensorPath, sensorConfig, generator);
   }
   else
   {
@@ -229,21 +231,21 @@ int main(int argc, char **argv)
   //   exit(0);
   // }
 
-  std::shared_ptr<SDF::SDF> sdf(new SDF::SDF());
+  std::shared_ptr<SDF::Generator> generator(new SDF::Generator());
   for (std::filesystem::path mav_path : std::filesystem::directory_iterator(input_path))
   {
     for (std::filesystem::path sensor_path : std::filesystem::directory_iterator(mav_path))
     {
       if (!std::filesystem::is_directory(sensor_path))
       {
-        processBody(sensor_path, sdf); // body.yaml
+        processBody(sensor_path, generator); // body.yaml
         continue;
       }
-      processSensor(sensor_path, sdf);
+      processSensor(sensor_path, generator);
     }
   }
 
-  sdf->write(output_path);
+  generator->write(output_path);
 
   return 0;
 }
